ef8b68b1b769445179c362fc7b2336809a967be5
kandoradev <kandoradev@gmail.com>
2023-11-13 09:45:34 -0400

    more requests

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Response, status
 from pydantic import BaseModel
 from typing import Optional
 from fastapi.params import Body
@@ -23,6 +23,12 @@ def find_post(id):
         if p["id"] == id:
             return p
         
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+        
 @app.get("/")
 async def root():
     return {"message": "I am happy to be here :)"}
@@ -31,24 +37,32 @@ async def root():
 def get_posts():
     return {"data": my_posts}
     
-@app.post("/posts{id}")
 def create_a_post(new_post: Post):
+    new_id = max(post["id"] for post in my_posts) + 1
     post_dict = new_post.dict()
-    post_dict['id'] = randrange(0, 100000000)
+    post_dict['id'] = new_id
     my_posts.append(post_dict)
     return {"data": post_dict}
 
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(new_post: Post):
+    return create_a_post(new_post)
+
 @app.delete("/posts/{id}")
 def delete_a_post(id: int):
-    post = find_post(post_id)
-    index = find_index_post(id)
-    myposts.pop(index)
+    post = find_post(id)
+    if not post:
+        return {"detail": f"Post with id {id} not found"}
+    index = my_posts.index(post)
+    my_posts.pop(index)
     return {"data": "post deleted"}
 
 @app.get("/posts/{id}")
-def get_posts(id: int):
-    print(id)
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Post with id {id} not found"}
     print(post)
-    return{"post_detail": post}
-    
+    return {"post_detail": post}
+
